85bdd582cfa8c95f74a07b1fa3895fa1cd89a76d
atlonis <veronika.belozechko@gmail.com>
2020-04-04 12:12:45 +0300

    converting a class component to a functional one

--- src/components/AuthModal/index.js
+++ src/components/AuthModal/index.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Text, View, Modal, TouchableHighlight } from 'react-native';
 
 import styles from './styles';
 
-export default class AuthModal extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    const { container, containerModal } = styles;
-    const { isShowPicker, togglePicker } = this.props;
-    return (
-      <Modal animationType="slide" visible={isShowPicker} transparent>
-        <View style={container}>
-          <View style={containerModal}>
-            <TouchableHighlight onPress={togglePicker}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-          </View>
+export default function Button(props) {
+  const { container, containerModal } = styles;
+  const { isShowPicker, togglePicker } = props;
+  return (
+    <Modal animationType="slide" visible={isShowPicker} transparent>
+      <View style={container}>
+        <View style={containerModal}>
+          <TouchableHighlight onPress={togglePicker}>
+            <Text>Hide Modal</Text>
+          </TouchableHighlight>
         </View>
-      </Modal>
-    );
-  }
+      </View>
+    </Modal>
+  );
 }
